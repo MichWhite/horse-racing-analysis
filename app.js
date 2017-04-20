db4cf9b7305f4c72abda9f840f0ef5f16cf7c781
Michealin White <mich.white@hotmail.com>
2017-04-20 18:38:05 +0100

    fix with heroku

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var cookieSession = require('cookie-session');
 // var echarts = require('echarts');
 
 var horses = require('./routes/horses');
@@ -28,7 +26,6 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
